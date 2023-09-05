e7d8f55a236127ff0de650cd8889539de5cae78e
Adam N. <51328998+adam-nx@users.noreply.github.com>
2023-09-05 22:29:55 +0100

    Wokrs?

---
+++ lua/adam/init.lua
@@ -0,0 +1,3 @@
+require("adam.set")
+require("adam.remap")
+
---
+++ lua/adam/packer.lua
@@ -0,0 +1,50 @@
+vim.cmd.packadd('packer.nvim')
+
+return require('packer').startup(function(use)
+    use 'wbthomason/packer.nvim'
+    use "ellisonleao/gruvbox.nvim"
+    use {
+        'nvim-telescope/telescope.nvim', 
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    use {
+        "folke/trouble.nvim",
+        config = function() require("trouble").setup { icons = false } end
+    }
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function()
+            local ts_update = require('nvim-treesitter.install')
+            .update({ with_sync = true })
+            ts_update()
+       end
+    }
+      use("theprimeagen/harpoon")
+      use("mbbill/undotree")
+      use("tpope/vim-fugitive")
+      use("nvim-treesitter/nvim-treesitter-context");
+
+      use {
+          'VonHeikemen/lsp-zero.nvim',
+          branch = 'v2.x',
+          requires = {
+              -- LSP Support
+              {'neovim/nvim-lspconfig'},
+              {'williamboman/mason.nvim'},
+              {'williamboman/mason-lspconfig.nvim'},
+
+              -- Autocompletion
+              {'hrsh7th/cmp-buffer'},
+              {'hrsh7th/cmp-nvim-lsp'},
+              {'hrsh7th/cmp-nvim-lua'},
+              {'hrsh7th/cmp-path'},
+              {'hrsh7th/nvim-cmp'},
+              {'saadparwaiz1/cmp_luasnip'},
+
+              -- Snippets
+              {'L3MON4D3/LuaSnip'},
+              {'rafamadriz/friendly-snippets'},
+          }
+      }
+end)
+
